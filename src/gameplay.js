363a4a4a6a83a418c9bc1e316df672518b589fe6
Daniel Savage <daniel@MacBook-Pro-2.local>
2018-08-08 22:20:14 -0700

    Basic dungeon movement

--- src/gameplay.js
+++ src/gameplay.js
@@ -3,6 +3,8 @@ var ControlsSettings = {
   ySensitivity: 1
 }
 
+var PlayerWalkSpeed = 50;
+
 var Gameplay = function () {
   this.mapKey = null;
   this.player = null;
@@ -17,10 +19,23 @@ Gameplay.prototype.preload = function () {
   this.game.load.tilemap('level_map', 'asset/map/' + this.mapKey + '.json', undefined, Phaser.Tilemap.TILED_JSON);
 };
 Gameplay.prototype.create = function() {
-  var s = this.game.add.sprite(this.game.width * 0.5, this.game.height * 0.5, 'test_sheet', 0);
-  s.anchor.set(0.5, 0.6);
-  s.scale.set(1);
-  s.update = function () { this.rotation -= 0.05; };
+  this.player = this.game.add.sprite(0, 0, 'test_sheet', 0);
+  this.player.data.gameState = this;
+  this.player.renderable = false;
+  this.player.update = function () {
+    if (this.game.input.keyboard.isDown(Phaser.KeyCode.W)) {
+      this.body.velocity.set(PlayerWalkSpeed * Math.cos(this.data.gameState.rotationY), PlayerWalkSpeed * Math.sin(this.data.gameState.rotationY));
+    } else if (this.game.input.keyboard.isDown(Phaser.KeyCode.S)) {
+      this.body.velocity.set(-PlayerWalkSpeed * Math.cos(this.data.gameState.rotationY), -PlayerWalkSpeed * Math.sin(this.data.gameState.rotationY));
+    } else if (this.game.input.keyboard.isDown(Phaser.KeyCode.A)) {
+      this.body.velocity.set(PlayerWalkSpeed * Math.cos(this.data.gameState.rotationY - Math.PI * 0.5), PlayerWalkSpeed * Math.sin(this.data.gameState.rotationY - Math.PI * 0.5));
+    } else if (this.game.input.keyboard.isDown(Phaser.KeyCode.D)) {
+      this.body.velocity.set(PlayerWalkSpeed * Math.cos(this.data.gameState.rotationY + Math.PI * 0.5), PlayerWalkSpeed * Math.sin(this.data.gameState.rotationY + Math.PI * 0.5));
+    } else {
+      this.body.velocity.set(0, 0);
+    }
+  };
+  this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
 
   this.rotationY = 0;
   this.game.input.addMoveCallback(function (pointer, x, y, isClickEvent, domMoveEvent) {
@@ -31,11 +46,20 @@ Gameplay.prototype.create = function() {
     }
   }, this);
 
+  var map = this.game.add.tilemap('level_map');
+  map.addTilesetImage('test', 'test_sheet_tile');
+  this.foreground = map.createLayer('walls');
+  this.foreground.resizeWorld();
+  this.foreground.renderable = false;
+  map.setCollisionByExclusion([0], true, this.foreground);
+  this.game.physics.enable(this.foreground, Phaser.Physics.ARCADE);
+
   populateThreeTestScene(this.game.cache.getTilemapData('level_map').data);
 };
 Gameplay.prototype.update = function() {
+  this.game.physics.arcade.collide(this.player, this.foreground);
 
-	renderThreeScene(this.rotationY);
+	renderThreeScene(this.player.centerX, this.player.centerY, this.rotationY);
 };
 Gameplay.prototype.shutdown = function() {
   this.game.cache.removeTilemap('level_map');
--- src/threeScene.js
+++ src/threeScene.js
@@ -21,7 +21,7 @@ var threeCanvas = undefined;
 	};
 
 	populateThreeTestScene = function (tilemapData) {
-		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+		var geometry = new THREE.BoxBufferGeometry( 32, 32, 32 );
 		var wallMaterial = new THREE.MeshBasicMaterial( { color: 0x5555ff } );
 		var floorMaterial = new THREE.MeshBasicMaterial( { color: 0x313233 } );
 		var ceilingMaterial = new THREE.MeshBasicMaterial( { color: 0x133333 } );
@@ -37,8 +37,8 @@ var threeCanvas = undefined;
 			};
 			var testMaterialHeights = {
 				walls: 0,
-				floor: -1,
-				ceiling: 1
+				floor: -32,
+				ceiling: 32
 			};
 			material = testMaterialMap[layer.name];
 			blockHeight = testMaterialHeights[layer.name];
@@ -53,7 +53,7 @@ var threeCanvas = undefined;
 
 
 				var cube = new THREE.Mesh( geometry, material );
-				cube.position.set(x * 1, blockHeight, y * 1);
+				cube.position.set(x * 32 + 16, blockHeight, y * 32 + 16);
 				scene.add( cube );
 			}, this);
 
@@ -64,7 +64,9 @@ var threeCanvas = undefined;
 		camera.position.y - 0.5;
 	};
 
-	renderThreeScene = function (rotationY) {
+	renderThreeScene = function (x, y, rotationY) {
+		camera.position.x = x;
+		camera.position.z = y;
 		var pos = new THREE.Vector3(camera.position.x + (10 * Math.cos(rotationY)), 0.5, camera.position.z + (10 * Math.sin(rotationY)));
 		camera.lookAt(pos);
 		renderer.render( scene, camera );
